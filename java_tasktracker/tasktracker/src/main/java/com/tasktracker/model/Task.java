3992836216bfe5f046a49539f2f172b757f5d8a4
mrczlmate <merczel259@gmail.com>
2025-05-22 12:07:11 +0200

    Create models

---
+++ java_tasktracker/tasktracker/src/main/java/com/tasktracker/model/Task.java
@@ -0,0 +1,74 @@
+package com.tasktracker.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status = TaskStatus.TO_DO;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
---
+++ java_tasktracker/tasktracker/src/main/java/com/tasktracker/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.tasktracker.model;
+
+public enum TaskStatus {
+    TO_DO,
+    IN_PROGRESS,
+    DONE
+}
